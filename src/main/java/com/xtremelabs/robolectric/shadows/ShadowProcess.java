6d9c2bb474ade43328e11df142601db9ff030333
Jon Boekenoogen <jboekeno@google.com>
2012-10-16 09:44:02 -0700

    Build Robolectric with Make
    Change-Id: Ie2d29ec56c16bcf04087a488274e312a9da3778c

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowProcess.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowProcess.java
@@ -3,8 +3,6 @@ package com.xtremelabs.robolectric.shadows;
 import com.xtremelabs.robolectric.internal.Implementation;
 import com.xtremelabs.robolectric.internal.Implements;
 
-import java.lang.management.ManagementFactory;
-
 @Implements(android.os.Process.class)
 public class ShadowProcess {
 
@@ -14,18 +12,6 @@ public class ShadowProcess {
     public static final int myPid() {
         if (pid != null) {
            return pid;
-        }
-
-        // Returns machine info in the form {pid}@{ComputerName}
-        String  machineId = ManagementFactory.getRuntimeMXBean().getName();
-
-        String[] parts = machineId.split("@");
-        if (parts.length == 2) {
-            try {
-                return Integer.parseInt(parts[0]);
-            } catch (NumberFormatException e) {
-                // Shouldn't occur on most systems, but default to zero in case it does.
-            }
         }
         return 0;
     }
